5e9c658a7073fb8a6b6bcee570f22355bcc3b90d
jonzski <jhperez4@up.edu.ph>
2024-04-21 16:47:08 +0800

    Clear changes

--- core/src/bombs/Bomb.java
+++
@@ -1,4 +0,0 @@
-package bombs;
-
-public class Bomb {
-}
--- core/src/scenes/MainMenu.java
+++ core/src/scenes/MainMenu.java
@@ -1,23 +1,10 @@
 package scenes;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
-import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
-import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import game.BoomPanes;
 
@@ -27,20 +14,6 @@ public class MainMenu extends ScreenAdapter {
     private final SpriteBatch batch;
     private FitViewport viewport;
     private Stage stage;
-
-    private BitmapFont font;
-
-    private Texture backgroundTexture;
-
-    private Texture indicationsTexture;
-    private Image indications;
-
-    private Image indicator0;
-    private Image indicator1;
-    private float indicatorX;
-    private float indicatorY;
-    private int currentSelection;
-    private boolean selected;
 
     public MainMenu(BoomPanes game) {
         this.game = game;
